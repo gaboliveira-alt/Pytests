c1968dca63c6ed4ed47cb65e7a47d1684d0a48c5
Gabriel Oliveira Pinto <157530699+gaboliveira-alt@users.noreply.github.com>
2026-04-14 22:13:25 -0300

    feat: update run_01.py and run_test_01.py

--- tests/run_01.py
+++ tests/run_01.py
@@ -1,7 +1,7 @@
 
-def fetch_discount_rate() -> float:
-    return 0.10
+def fetch_discount_rate(initial_value: float) -> float:
+    return 0.10 * initial_value
 
 def get_discount(price: float) -> float:
-    discount_rate = fetch_discount_rate()
+    discount_rate = fetch_discount_rate(0.9)
     return price - (price * discount_rate)
--- tests/run_test_01.py
+++ tests/run_test_01.py
@@ -5,3 +5,23 @@ def test_get_discount(mocker):
     
     response = run_01.get_discount(100)
     assert response == 50
+
+def test_get_discount_with_spy_integration(mocker):
+    spy = mocker.spy(run_01, "fetch_discount_rate")
+    response = run_01.get_discount(100)
+    
+    assert response == 91.0
+    spy.assert_called_once()
+    spy.assert_called_with(0.9)
+
+def fake_discount_rate(initial_value: float):
+    return 0.40
+
+def test_get_discount_with_spy_and_custom_return(mocker):
+    spy = mocker.patch("tests.run_01.fetch_discount_rate", side_effect=fake_discount_rate)
+    
+    result = run_01.get_discount(100)
+    
+    assert result == 60.0
+    spy.assert_called_once()
+    spy.assert_called_with(0.9)
